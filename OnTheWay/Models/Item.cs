f0b483c0b0bf6470ba64feb78f92f10a2296ae66
nelsoncapo <42743897+nelsoncapo@users.noreply.github.com>
2020-07-26 21:57:10 -0400

    Add files via upload

---
+++ OnTheWay/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Diagnostics;
+
+namespace OnTheWay.Models
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
---
+++ data base/UserInfo/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Login_Email] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(50) NULL, 
+    [LName] NVARCHAR(50) NULL, 
+    [FName] NVARCHAR(50) NULL, 
+    [Status ] NTEXT NULL, 
+    [Last_Login_Date] DATE NULL, 
+    [Phone] NVARCHAR(50) NULL, 
+    [Location] INT NULL, 
+    [Location_GPS] [sys].[geography] NULL
+)
