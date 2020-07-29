36b816976e2a1f347d414cc2a27fc23700a3b5a3
nelsoncapo <42743897+nelsoncapo@users.noreply.github.com>
2020-07-28 22:11:01 -0400

    Add files via upload

---
+++ Services/ApiServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnTheWay.Services
+{
+    public class ApiServices
+    {
+         
+    }
+}
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnTheWay.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
@@ -8,6 +9,7 @@ namespace OnTheWay.ViewModels
 {
     public class RegisterViewModel
     {
+        ApiServices apiservices = new ApiServices();
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
@@ -18,6 +20,7 @@ namespace OnTheWay.ViewModels
             {
                 return new Command(() =>
                {
+                   apiservices.RegisterAsync
 
                });
             }
